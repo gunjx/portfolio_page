d0786f830958330e9cd2df4547d9affbf1d1df08
gunjx <tommy.halks@gmail.com>
2018-02-22 18:54:37 +0100

    Move all javascript from html to scripts.js. Style form with javascript. Show user input in DOM with event listener and hide form after submission.

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,73 @@
-function showInput() {
-  document.getElementById("showName").innerHTML = document.getElementById("name").value;
-  document.getElementById("showEmail").innerHTML = document.getElementById("email").value;
-  document.getElementById("showMessage").innerHTML = document.getElementById("message-box").value;
-}
+"use strict";
 
-// function confirm() {
-//   var confirmation = document.createTextNode("Your message has been sent. Thank you!");
-//   var p = document.createElement('p');
-//   p.appendChild(userInput);
-//   document.getElementById('userInput').appendChild(p);
-// }
+// Twitter follow button
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 
-// $("btn-submit").click(function () {
-//   var confirmation = document.createTextNode("Your message has been sent. Thank you!");
-//   var p = document.createElement('p');
-//   p.appendChild(userInput);
-//   document.getElementById('userInput').appendChild(p);
-// });
+// Facebook like buttom
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=535621223243648&autoLogAppEvents=1'; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));
 
-// function processUserData(){
-//   var userName = document.getElementById("name").value;
-//   var p = document.createElement('p');
-//   var i = document.createTextNode(userName);
-//   p.appendChild(i);
-//   document.getElementById('userInput').appendChild(p);
-//
-//   var userEmail = document.getElementById("email").value;
-//   var p = document.createElement('p');
-//   var i = document.createTextNode(userEmail);
-//   p.appendChild(i);
-//   document.getElementById('userInput').appendChild(p);
-//
-//   var userMessage = document.getElementById("message-box").value;
-//   var p = document.createElement('p');
-//   var i = document.createTextNode(userMessage);
-//   p.appendChild(i);
-//   document.getElementById('userInput').appendChild(p);
-// }
+$(document).ready(function(){ // Wait for DOM to load
+
+  // Smooth scrolling
+  var $root = $('html, body');
+  $('.navbar-nav a').click(function() {
+    var href = $.attr(this, 'href');
+    if (href != undefined && href != '#') {
+      $root.animate({
+        scrollTop: $(href).offset().top
+      }, 500, function () {
+        window.location.hash = href;
+      });
+    }
+    return false;
+  });
+
+  // Modify script to include home button
+  $('.gototop').on('click', function (event) {
+    event.preventDefault();
+    $('html,body').animate( {
+      scrollTop: 0}, 500);
+  });
+
+  // Tooltip script
+  $(function () {
+    $('.tooltip--link').tooltip();
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
+  // Style message-box red
+  $(".form-control").css("background-color", "rgba(242, 250, 167, 0.5)");
+  $(".form-control").css("opacity", "100%");
+
+ // Show user input with event listener
+  $("#btn-submit").on("click", function() {
+    let name = $("#name").val();
+    let email = $("#email").val();
+    let message = $("#message-box").val();
+    $("#thankyou").html("Thank you for your message!");
+    $("#showName").html("Name: " + name);
+    $("#showEmail").html("Email: " + email);
+    $("#showMessage").html("Message: " + message);
+    $(".form").hide();
+    return false;
+  });
+
+  // Show user input using html tag onclick(showInput;)
+  // function showInput() {
+  // document.getElementById("showName").innerHTML = document.getElementById("name").value;
+  //   document.getElementById("showEmail").innerHTML = document.getElementById("email").value;
+  //   document.getElementById("showMessage").innerHTML = document.getElementById("message-box").value;
+  // }
+
+  // Show user input using appendChild
+  // function processUserData(){
+  //   let userName = document.getElementById("name").value;
+  //   let p = document.createElement('p');
+  //   let i = document.createTextNode(userName);
+  //   p.appendChild(i);
+  //   document.getElementById('userInput').appendChild(p);
+  // }
+
+}); // End document.ready
